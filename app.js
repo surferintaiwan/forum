026ca83c6ff33854e6284048ff90ec48a44d6c9e
Shawn <pan.conan@gmail.com>
2020-02-12 19:03:46 +0800

    confirm user's unique email and password(use express-session and connect-flash)

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
-
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+}))
+app.use(flash())
+
+app.use((req,res,next) => {
+    res.locals.success_messages = req.flash('success_messages')
+    res.locals.error_messages = req.flash('error_messages')
+    next()
+})
 
 app.listen(port, ()=>{
     db.sequelize.sync()
--- controllers/userController.js
+++ controllers/userController.js
@@ -10,22 +10,44 @@ const userController = {
         name = req.body.name
         email = req.body.email
         password = req.body.password
-        bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(password, salt, function(err, hash) {
-                // Store hash in your password DB.
-                const newUser = new User({
-                    name: name,
-                    email: email,
-                    password: hash
+        passwordCheck = req.body.passwordCheck
+        // 驗證兩次密碼是否相同
+        if (password !== passwordCheck) {
+            req.flash('error_messages', '兩次密碼不相符')
+            return res.redirect('/signup')
+        } else {
+        // 查詢帳號是否已註冊
+            User.findOne({where: {email: email}})
+                .then(user=>{
+                    // 有註冊過就跳出error訊息
+                    if (user) {
+                        req.flash('error_messages', '這個帳號註冊過囉!')
+                        return res.redirect('/signup')
+                    } else {
+                        // 沒註冊過則存進資料庫
+                        console.log()
+                        bcrypt.genSalt(10, function(err, salt) {
+                            bcrypt.hash(password, salt, function(err, hash) {
+                                // Store hash in your password DB.
+                                const newUser = new User({
+                                    name: name,
+                                    email: email,
+                                    password: hash
+                                })
+                                newUser
+                                .save()
+                                .then(user => {
+                                    req.flash('success_messages', '註冊成功囉!')
+                                    return res.redirect('/signin')
+                                })
+                            })
+                        })
+                    }     
                 })
-                newUser
-                .save()
-                .then(user => {
-                    res.redirect('/signin')
-                })
-            })
-            
-        })
+        }
+
+        
+        
     }
 }
 
