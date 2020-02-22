324542f7b605f3c62212355f45d9d1b7f108e11d
Shawn <pan.conan@gmail.com>
2020-02-22 18:25:08 +0800

    add nav bar of categories in restaurants index page(with req.query.category.id)

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,7 +4,15 @@ const Category = db.Category
 
 module.exports = {
     getRestaurants: (req, res) => {
-        Restaurant.findAll({ include: Category })
+        // 如果選擇"全部"分類，categoryId就會是一開始宣告的空字串，就不會帶條件進去findAll裡面撈資料        
+        let whereQuery = {}
+        let categoryId = ''
+        if (req.query.categoryId) {
+            categoryId = Number(req.query.categoryId)
+            whereQuery['CategoryId'] = categoryId
+            console.log(whereQuery)
+        }
+        Restaurant.findAll({ include: Category, where: whereQuery})
                     .then(restaurants => {
                         // 把description拿出來處理，變成50個字元再存進去，最後給view用
                         const data = restaurants.map(r => { 
@@ -12,10 +20,14 @@ module.exports = {
                             ...r.dataValues,
                             description: r.description.substring(0, 50)
                         }})
-                        console.log(data)
-                        return res.render('restaurants', {
-                            restaurants: JSON.parse(JSON.stringify(data))
-                        })
+                        Category.findAll({raw: true})
+                                .then(categories => {
+                                    return res.render('restaurants', {
+                                        restaurants: JSON.parse(JSON.stringify(data)),
+                                        categories: categories,
+                                        categoryId: categoryId
+                                    })
+                                })
                     })
     },
     getRestaurant: (req, res) => {
