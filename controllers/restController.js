d2ed3db1cc4d7900a14b5c5b099c1f3a1c847b6c
Shawn <pan.conan@gmail.com>
2020-02-22 16:57:14 +0800

    correct the data to make the category appear in restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,18 +4,20 @@ const Category = db.Category
 
 module.exports = {
     getRestaurants: (req, res) => {
-        Restaurant.findAll({ include: Category }).then(restaurants => {
-            // 把description拿出來處理，變成50個字元再存進去，最後給view用
-            const data = restaurants.map(r => { 
-                return {
-                ...r.dataValues,
-                description: r.description.substring(0, 50)
-            }})
-            return res.render('restaurants', {
-            restaurants: data
-          })
-        })
-      }
+        Restaurant.findAll({ include: Category })
+                    .then(restaurants => {
+                        // 把description拿出來處理，變成50個字元再存進去，最後給view用
+                        const data = restaurants.map(r => { 
+                            return {
+                            ...r.dataValues,
+                            description: r.description.substring(0, 50)
+                        }})
+                        console.log(data)
+                        return res.render('restaurants', {
+                            restaurants: JSON.parse(JSON.stringify(data))
+                        })
+                    })
+    }
 }
 
 /*
