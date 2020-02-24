0cea696f57a256b0c38c6646f52dcadbe0dd43eb
Shawn <pan.conan@gmail.com>
2020-02-24 22:22:38 +0800

    Add addFavorite and removeFavorite feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const db = require('../models')
 const User = db.User
 const Comment = db.Comment
 const Restaurant = db.Restaurant
+const Favorite = db.Favorite
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 /* 如果是想要把temp資料夾內的圖片複製到upload就要載入fs
@@ -133,6 +134,30 @@ const userController = {
                     })
                 })
         }
+    },
+    addFavorite: (req, res) => {
+        return Favorite.create({
+            UserId: req.user.id,
+            RestaurantId: req.params.restaurantId
+        })
+        .then((favorite)=> {
+            return res.redirect('back')
+        })
+    },
+    removeFavorite: (req, res) => {
+        return Favorite.findOne({
+            where: {
+                UserId: req.user.id,
+                RestaurantId: req.params.restaurantId
+            }
+        })
+        .then(favorite => {
+            favorite.destroy()
+                    .then(favorite => {
+                        return res.redirect('back')
+                    })
+            
+        })
     }
 }
 
